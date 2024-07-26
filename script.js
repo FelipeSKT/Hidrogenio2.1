664c251112e41d4383383d76e5594be7d899bbec
FelipeSKT <107480837+FelipeSKT@users.noreply.github.com>
2024-07-26 02:27:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const modoNoturnoIcon = document.getElementById('modo-noturno');
+    const overlay = document.getElementById('overlay');
+    const closeOverlay = document.getElementById('close-overlay');
+    const centralButton = document.querySelector('.central-button');
+    
+    // Inicialmente, a sobreposição deve estar oculta
+    overlay.style.display = 'none';
+    
     document.getElementById('modo-noturno').addEventListener('click', function() {
         document.body.classList.toggle('modo-noturno');
+        
+        // Trocar o ícone baseado no modo noturno
+        if (document.body.classList.contains('modo-noturno')) {
+            modoNoturnoIcon.src = 'icone_sol.png'; // Substitua com o caminho da imagem do sol
+        } else {
+            modoNoturnoIcon.src = 'icone_lua.png'; // Substitua com o caminho da imagem da lua
+        }
+    });
+    
+    centralButton.addEventListener('click', function() {
+        overlay.style.display = 'flex'; // Use 'flex' para exibir a sobreposição centralizada
+    });
+    
+    closeOverlay.addEventListener('click', function() {
+        overlay.style.display = 'none';
+    });
+    
+    // Fechar a sobreposição quando clicar fora dela
+    overlay.addEventListener('click', function(event) {
+        if (event.target === overlay) {
+            overlay.style.display = 'none';
+        }
     });
 
     window.addEventListener('scroll', function() {
@@ -11,29 +41,8 @@ document.addEventListener('DOMContentLoaded', function() {
             footer.style.bottom = '-150px';
         }
     });
+});
 
-
-        const overlay = document.getElementById('overlay');
-        const closeOverlay = document.getElementById('close-overlay');
-        const centralButton = document.querySelector('.central-button');
     
-        // Inicialmente, a sobreposição deve estar oculta
-        overlay.style.display = 'none';
-    
-        centralButton.addEventListener('click', function() {
-            overlay.style.display = 'flex'; // Use 'flex' para exibir a sobreposição centralizada
-        });
-    
-        closeOverlay.addEventListener('click', function() {
-            overlay.style.display = 'none';
-        });
-    
-        // Fechar a sobreposição quando clicar fora dela
-        overlay.addEventListener('click', function(event) {
-            if (event.target === overlay) {
-                overlay.style.display = 'none';
-            }
-        });
-    });
     
 
